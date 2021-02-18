c2aa3b10dcba4c7db7afa6141c68aff54ecbda3c
Prof Matias Garcia <54009386+profmatiasgarcia@users.noreply.github.com>
2021-02-18 20:01:05 -0300

    Add files via upload

---
+++ src/main/java/Clase2Ej4/Test.java
@@ -0,0 +1,43 @@
+package Clase2Ej4;
+/**
+ * @author Prof Matias Garcia.
+ * <p> Copyright (C) 2017 para <a href = "https://www.profmatiasgarcia.com.ar/"> www.profmatiasgarcia.com.ar </a>
+ * - con licencia GNU GPL3.
+ * <p> Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo los términos de la
+ * Licencia Pública General de GNU según es publicada por la Free Software Foundation, 
+ * bien con la versión 3 de dicha Licencia o bien (según su elección) con cualquier versión posterior. 
+ * Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA GARANTÍA, 
+ * incluso sin la garantía MERCANTIL implícita o sin garantizar la CONVENIENCIA PARA UN PROPÓSITO
+ * PARTICULAR. Véase la Licencia Pública General de GNU para más detalles.
+ * Debería haber recibido una copia de la Licencia Pública General junto con este programa. 
+ * Si no ha sido así ingrese a <a href = "http://www.gnu.org/licenses/"> GNU org </a>
+ */
+import javax.swing.JOptionPane;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Persona vectorPersonas[] = new Persona[3];
+        String nom;
+        long dni;
+        int edad;
+        for (int i = 0; i < vectorPersonas.length; i++) {
+            nom = JOptionPane.showInputDialog("Ingrese nombre");
+            dni = Long.parseLong(JOptionPane.showInputDialog("Ingrese su DNI"));
+            edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su edad"));
+            vectorPersonas[i] = new Persona(nom, dni, edad);
+        }
+
+        String result = "";
+        for (int i = 0; i < vectorPersonas.length; i++) {
+            result += vectorPersonas[i].datos() + "\n";
+        }
+        
+//        for (Persona vectorPersona : vectorPersonas) {
+//            result += vectorPersona.datos() + "\n";
+//        }
+
+        JOptionPane.showMessageDialog(null, result);
+
+    }
+}
